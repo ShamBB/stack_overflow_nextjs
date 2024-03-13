f2564a3c42ba5f6ccd0c6fb60b1205a28917b9bf
ShamBB <sham_berani@yahoo.com>
2024-03-13 15:36:33 +0800

    fix routing

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "question/:id",
+    "/question/:id",
     "/tags",
     "/tags/:id",
-    "/profile:id",
+    "/profile/:id",
     "/community",
     "/jobs",
   ],
