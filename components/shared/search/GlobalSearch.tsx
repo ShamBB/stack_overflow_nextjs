95436f15c4a32fdbe37be810b7f2084ecaf52094
ShamBB <sham_berani@yahoo.com>
2024-04-05 23:39:58 +0800

    global search first commit

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,8 +1,42 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const GlobalSearch = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const [globalSearch, setGlobalSearch] = useState("");
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (globalSearch) {
+        router.push(pathname + "?" + createQueryString("global", globalSearch));
+      } else {
+        router.push(pathname);
+      }
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [globalSearch]);
+
+  function handleChangeSearch(e: React.KeyboardEvent<HTMLInputElement>) {
+    const textInput = e.target as HTMLInputElement;
+    const textValue = textInput?.value.trim();
+    setGlobalSearch(textValue);
+  }
+
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div
@@ -22,6 +56,7 @@ const GlobalSearch = () => {
           className="paragraph-regular no-focus placeholder 
           background-light800_darkgradient 
           text-light400_light500 border-none shadow-none outline-none"
+          onKeyUp={(e) => handleChangeSearch(e)}
         />
       </div>
     </div>
