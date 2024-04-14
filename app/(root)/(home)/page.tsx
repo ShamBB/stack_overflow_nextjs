0f6e4714060b2e790ec27a9a2d67abdd5a35a9c8
ShamBB <sham_berani@yahoo.com>
2024-04-14 12:39:05 +0800

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -18,6 +18,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
     page: searchParams.page ? +searchParams.page : 1,
   });
   const { userId: clerkId } = auth();
+  // test
   return (
     <>
       <div
